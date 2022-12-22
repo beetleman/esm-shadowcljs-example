b2c076933344128fd204224dba35622bfc6e400c
Mateusz Probachta <Mateusz.Probachta@vertexinc.com>
2022-12-22 14:00:54 +0100

    prepare `esm` target example

--- src/acme/app.cljs
+++ src/acme/app.cljs
@@ -1,4 +1,22 @@
-(ns acme.app)
+(ns acme.app
+  (:require [reagent.dom :as rdom]
+            [reagent.core :as reagent]))
 
-(defn ^:export init []
-  (println "Hello World"))
+(defn hello-world [{:keys [user-name]}]
+  [:div
+   [:h1 "Hello " user-name "!"]])
+
+(defn mount [el state]
+  (rdom/render [hello-world @state] el))
+
+(defonce state (reagent/atom {:user-name ""}))
+(defonce root-el (atom nil))
+
+(defn ^:dev/after-load on-reload []
+  (mount @root-el state))
+
+(defn init [opts]
+  (let [{:keys [root userName] :as x} (js->clj opts :keywordize-keys true)]
+    (reset! state {:user-name userName})
+    (reset! root-el root)
+    (mount @root-el state)))
